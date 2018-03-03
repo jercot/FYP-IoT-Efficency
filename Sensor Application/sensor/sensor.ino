728a647fedc26e5d56c48f955473ae1846f0af4b
Jeremiah Cotter <jeremiahcotter@gmail.com>
2018-03-03 02:31:43 +0000

    Save/Read new bucket

--- Sensor Application/sensor/sensor.ino
+++ Sensor Application/sensor/sensor.ino
@@ -3,13 +3,14 @@
 #include <Ethernet.h>
 #include <EEPROM.h>
 #include <SPI.h>
+#include <EEPROM.h>
 
 #define DHT11_PIN 7
 #define TRIG_PIN 6
 #define ECHO_PIN 5
 #define TEMP_PIN 0
 #define LIGHT_PIN 1
-#define READINGS 20
+#define READINGS 30
 #define VARIANCE 2
 #define MINUTES 10
 #define GREEN 9
@@ -24,9 +25,8 @@ int hum[READINGS];
 int ult[READINGS];
 float temp[READINGS];
 int light[READINGS];
-byte mac[] = { 0xDE, 0xAD, 0xBE, 0xEF, 0xFE, 0xED };
-char webServer[] = "fyp-iot-efficiency.eu-west-1.elasticbeanstalk.com";    // AWS
-char bucket[25] = "123JA423SD4A23FX";
+byte mac[] = {0xDE, 0xAD, 0xBE, 0xEF, 0xFE, 0xED};
+char bucket[20];
 EthernetClient client;
 EthernetServer arduinoServer(80);
 
@@ -41,8 +41,10 @@ void setup() {
     digitalWrite(RED, LOW);
   }
   arduinoServer.begin();
-  Serial.print("Arduino Server is at ");
   Serial.println(Ethernet.localIP());
+  !readBucket();
+  for(int i=0;i<20;i++)
+    Serial.print(bucket[i]);
 }
 
 void loop() {
@@ -50,40 +52,13 @@ void loop() {
   incr();
   digitalWrite(GREEN, LOW);
   if(count==READINGS) {
-    digitalWrite(GREEN, HIGH);
-    char str[255];
-    char humS[72];
-    char lightS[72];
-    char tempS[72];
-    char aveF[6], medF[6], minS[6], maxS[6];
-    int minI, maxI;
-    float minF, maxF;
-    int med = calcMed(hum, &minI, &maxI);
-    sprintf(humS, "&humAve=%d&humMed=%d&humMin=%d&humMax=%d", calcAve(hum), med, minI, maxI);
-    med = calcMed(light, &minI, &maxI);
-    sprintf(lightS, "&lightAve=%d&lightMed=%d&lightMin=%d&lightMax=%d", calcAve(light), med, minI, maxI);
-    dtostrf(calcAveF(temp), 4, 2, aveF);
-    dtostrf(calcMedF(temp, &minF, &maxF), 4, 2, medF);
-    dtostrf(minF, 4, 2, minS);
-    dtostrf(maxF, 4, 2, maxS);
-    sprintf(tempS, "&tempAve=%s&tempMed=%s&tempMin=%s&tempMax=%s", aveF, medF, minS, maxS);
-    sprintf(str, "/upload?movement=%d%s%s%s&bucket=%s", calcMov(ult), humS, lightS, tempS, bucket);
-    Serial.println(str);
-    if(!upload(webServer, str, 3)) {
-      digitalWrite(RED, HIGH);
-    }
-    else {
-      digitalWrite(RED, LOW);
-    }
-    delay(1000);
-    count=0;
+    createRequest();
   }
-  //delay((MINUTES*60000)/READINGS);
   flash((MINUTES*60)/READINGS);
 }
 
 void flash(int c) {
-  settings();
+  server();
   digitalWrite(YELLOW, HIGH);
   delay(500);
   digitalWrite(YELLOW, LOW);
@@ -92,51 +67,119 @@ void flash(int c) {
     flash(c-1);
 }
 
-int settings() {
+byte readBucket() {
+  for(int i=0; i<20; i++) {
+    //if(req[j]>='0'&&req[j]<='9')
+      bucket[i] = EEPROM.read(i);
+    //else return 0;
+  }
+  return 1;
+}
+
+int server() {
   client = arduinoServer.available();
   if (client) {
     if (client.connected()) {
-      int i = 0, k = 25;
-      String param;
-      while (client.available() && i < k) {
+      char req[42];
+      int i = 0;
+      while (client.available() && i < sizeof(req)) {
         char c = client.read();
-        param += c;
-        i += 1;
+        req[i] = c;
+        i++;
       }
-      int s = param.indexOf("/")+1;
-      param = param.substring(s, param.indexOf(" ", s));
-      s = param.indexOf("?")+1;
-      int d = param.indexOf("=")+1;
-      if(s!=0 && d!=0) {
-        String nameA = param.substring(s, d-1);
-        String value = param.substring(" ", d);
-        if(nameA.compareTo("update")==0)
-          updateBucket(nameA, value);
-        else
-          client.print("3, ");
+      char set[] = "settings", var[] = "bucket";
+      if (!request('/', req, set))
+          client.print("-1");
+      else if (!request('?', req, var)) {
+          client.print("1, ");
+          client.print(bucket);
       }
-      else
-        client.print("1, ");
-      client.print(bucket);
+      else {
+        client.print("UPDATED");
+        updateBucket(req);
+      }
     }
     client.stop();
   }
 }
 
-void updateBucket(String nameA, String value) {
-  client.print("2, ");
+byte request(char start, char *req, char *check) {
+  byte compare = 1;
+  int j=0, i=0;
+  while(req[j]!='\0'&&req[j-1]!=start) {
+    j++;
+  }
+  while(req[i+j]!='\0'&&check[i]!='\0')  {
+    if(req[i+j]!=check[i])
+      compare = 0;
+    i++;
+  }
+  return compare;
 }
 
-byte upload(char *ipBuf, char *param, int attempt) {
+int availableMemory() {
+    // Use 1024 with ATmega168
+    int size = 2048;
+    byte *buf;
+    while ((buf = (byte *) malloc(--size)) == NULL);
+        free(buf);
+    return size;
+}
+
+void updateBucket(char *req) {
+  int j=0, i=0;
+  while(req[j]!='\0'&&req[j-1]!='=') {
+    j++;
+  }
+  while(req[j]!='\0'&&req[j]!=' '&&j<20) {
+    EEPROM.write(i, req[j]);
+    bucket[i] = EEPROM.read(i);
+    i++;
+    j++;
+  }
+}
+
+void createRequest() {
+  digitalWrite(GREEN, HIGH);
+  char str[255];
+  char humS[60];
+  char lightS[60];
+  char tempS[60];
+  char aveF[6], medF[6], minS[6], maxS[6];
+  int minI, maxI;
+  float minF, maxF;
+  int med = calcMed(hum, &minI, &maxI);
+  sprintf(humS, "&humAve=%d&humMed=%d&humMin=%d&humMax=%d", calcAve(hum), med, minI, maxI);
+  med = calcMed(light, &minI, &maxI);
+  sprintf(lightS, "&lightAve=%d&lightMed=%d&lightMin=%d&lightMax=%d", calcAve(light), med, minI, maxI);
+  dtostrf(calcAveF(temp), 4, 2, aveF);
+  dtostrf(calcMedF(temp, &minF, &maxF), 4, 2, medF);
+  dtostrf(minF, 4, 2, minS);
+  dtostrf(maxF, 4, 2, maxS);
+  sprintf(tempS, "&tempAve=%s&tempMed=%s&tempMin=%s&tempMax=%s", aveF, medF, minS, maxS);
+  sprintf(str, "/upload?movement=%d%s%s%s&bucket=%s", calcMov(ult), humS, lightS, tempS, bucket);
+  Serial.println(str);
+  if(!upload(str, 3)) {
+    digitalWrite(RED, HIGH);
+  }
+  else {
+    digitalWrite(RED, LOW);
+  }
+  delay(1000);
+  count=0;
+}
+
+byte upload(char *param, int attempt) {
+  char webServer[] = "fyp-iot-efficiency.eu-west-1.elasticbeanstalk.com"; 
   int inChar;
   char outBuf[64];
-  if(client.connect(ipBuf,80)) {
+  if(client.connect(webServer,80)) {
     sprintf(outBuf,"GET %s HTTP/1.0\r\n\r\n", param);
     client.write(outBuf);
   } 
   else {
     if(attempt>3)
-      upload(ipBuf, param, attempt-1);
+      upload(param, attempt-1);
     return 0;
   }
   client.stop();
