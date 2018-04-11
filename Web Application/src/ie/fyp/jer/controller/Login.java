966d6ba87503ec27880a9c7527523d374f42ef07
Jeremiah Cotter <j.cotter@mycit.ie>
2018-04-11 17:11:37 +0100

    Save location/os/browser when logging in.

--- Web Application/src/ie/fyp/jer/controller/Login.java
+++ Web Application/src/ie/fyp/jer/controller/Login.java
@@ -14,7 +14,16 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.sql.DataSource;
 
+import org.apache.http.HttpEntity;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.util.EntityUtils;
 import org.mindrot.jbcrypt.BCrypt;
+
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
 
 import ie.fyp.jer.domain.Logged;
 
@@ -65,6 +74,7 @@ public class Login extends HttpServlet {
 					if(BCrypt.checkpw(password, rs.getString(3))) {
 						Logged log = new Logged(rs.getString(1), rs.getInt(2));
 						sql = "SELECT name FROM FYP.building WHERE accountId = ?";
+						String location = "Unknown";
 						Object val2[] = {log.getId()};
 						try (PreparedStatement ptst1 = prepare(con, sql, val2);
 								ResultSet rs1 = ptst1.executeQuery()) {
@@ -72,8 +82,24 @@ public class Login extends HttpServlet {
 								log.addBuilding(rs1.getString(1));
 							request.getSession().setAttribute("logged", log);
 						}
-						Object val3[] = {log.getId(), System.currentTimeMillis(), request.getRemoteAddr()};
-						sql = "INSERT INTO FYP.Login(accountid, datetime, location)VALUES (?, ?, ?);";
+						CloseableHttpClient httpclient = HttpClients.createDefault();
+						HttpGet httpGet = new HttpGet("http://ip-api.com/json" + request.getRemoteAddr());
+						CloseableHttpResponse response1 = httpclient.execute(httpGet);
+						try {
+							HttpEntity entity1 = response1.getEntity();
+							String gson = EntityUtils.toString(entity1);
+							JsonParser parse = new JsonParser();
+							JsonObject object = parse.parse(gson).getAsJsonObject();
+							location = (object.get("city").getAsString() + " " + object.get("countryCode").getAsString());
+							EntityUtils.consume(entity1);
+						} catch (Exception e) {
+							e.printStackTrace();
+						} finally {
+						    response1.close();
+						}
+						String  browserDetails = request.getHeader("User-Agent");
+						Object val3[] = {log.getId(), System.currentTimeMillis(), location, browserDetails};
+						sql = "INSERT INTO FYP.Login(accountid, datetime, location, osbrowser)VALUES (?, ?, ?, ?);";
 						try (PreparedStatement ptst1 = prepare(con, sql, val3)) {
 							ptst1.executeUpdate();
 						}
