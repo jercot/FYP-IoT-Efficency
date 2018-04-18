35d65d89e6ff1ef860e185cb6a1e30f8a4dbc0e9
Jeremiah Cotter <j.cotter@mycit.ie>
2018-04-19 00:55:35 +0100

    Visualisation

--- Web Application/WebContent/WEB-INF/house.jsp
+++ Web Application/WebContent/WEB-INF/house.jsp
@@ -2,12 +2,33 @@
 <%@ taglib prefix="fn" uri="http://java.sun.com/jsp/jstl/functions"%>
 <c:choose>
 	<c:when test="${not empty message}">
-		<c:out value="${message}"/><hr>
+		<c:out value="${message}"/>
 	</c:when>
 </c:choose>
 <div id="graph">
-	<div id="roomChart"></div>
+	<div id="chartSwitch">
+		<svg xmlns="http://www.w3.org/2000/svg" viewbox="0 0 100 300" id="switchThermo">
+			<circle cx="50" cy="245" r="33" stroke="none" />
+			<path d="M34,40 34,223 a31,31 0 1,0 32,0 L66,40 a1,1 0 0,0 -32,0" 
+			 fill="none" stroke="white" stroke-width="3px"/>
+			<path d="M30,40 30,220 a35,35 0 1,0 40,0 L70,40 a1,1 0 0,0 -40,0" 
+			 fill="none" stroke="black" stroke-width="6px"/>
+			<path d="M70,200 55,200 M70,180 55,180 M70,160 55,160 
+			M70,140 55,140 M70,120 55,120 M70,100 55,100 
+			M70,80 55,80 M70,60 55,60" stroke="black" stroke-width="2px"/>
+		</svg>
+		<svg xmlns="http://www.w3.org/2000/svg" viewbox="0 0 200 300" id="switchBulb">
+			<path d="M60,210 60,270 a10,1 0 0,0 80,0 L140,210" stroke="black" fill="grey" stroke-width="6px"/>
+			<path d="M140,220 60,230 M140,240 60,250 M140,260 60,270" stroke="black" fill="grey" stroke-width="3px"/>
+			<path d="M20,140 a90,90 0 1,1 160,0 L140,210 a8,1 0 1,1 -80,0 L20,140" stroke="black" stroke-width="6px" />
+		</svg>
+		<div id="topCharts">
+			<div id="roomChart"></div>
+			<div id="dayChart"></div>
+		</div>
+	</div>
 	<div id="readingChart"></div>
+	<div id="dragChart"></div>
 </div>
 
 <div id="roomList">
--- Web Application/WebContent/javascript/custom.js
+++ Web Application/WebContent/javascript/custom.js
@@ -1,8 +1,13 @@
 var roomChart = dc.pieChart("#roomChart");
+var dayChart = dc.rowChart("#dayChart");
 var readingChart = dc.lineChart("#readingChart");
+var dragChart = dc.barChart("#dragChart");
+var switchThermo = $("#switchThermo");
+var switchBulb = $("#switchBulb");
 var sensor = 0;
 var saveData = null;
 var color = ["#8abee2", "#3cbe98", "#f99e18", "#fe2918", "#fcd812", "#c7d892"];
+var filter;
 
 function startVisual(house) {
 	$.post("data", {bName: house},function(data) {
@@ -12,122 +17,193 @@ function startVisual(house) {
 	.fail(function(d) {
 		console.log("Post Request Failed");
 	});
-	/*$.ajax({
-	    type : 'POST',
-	    url : "data",
-	    data : {bName: house},
-	    dataType : "json",
-	    success : function(data) {
-	    	createGraph(data);
-	    }
-	});*/
 }
 
 function switchStatement(data) {
 	switch(data.code) {
 	case 1:
 		saveData = data.records;
-		createGraph(data.records);
+		setStats(data.records);
 		break;
 	}
 }
 
-function createGraph(data) {
-    var numberFormat = d3.format('.2f');
+function setStats(data) {
 	data.forEach(function (d) {
 		var date = new Date(+d.ti);
-		var formatTime = d3.time.format("%d/%m/%Y");
-		d.date = formatTime(date);
+		var formatTime = d3.time.format("%A");
+		d.day = formatTime(date);
 		d.month = d3.time.month(date);
 		d.hour = d3.time.hour(date);
 		d.min = d3.time.minute.range(d.hour, date, 15);
 		d.min = d.min[d.min.length-1]
 	});
-	scale = [getDate(data[0].date, 1), getDate(data[data.length-1].date, 0)];
-	var filter = crossfilter(data),
+	graphScale = [new Date(+data[0].ti-9000000), new Date(+data[data.length-1].ti+9000000)];
+	filter = crossfilter(data),
 	roomDimension = filter.dimension(function (d) {
 		return d.na;
 	}),
 	roomGroup = roomDimension.group().reduceSum(function(d) {
-		return +1;
+		return 1;
 	}),
 	readingDimension = filter.dimension(function (d) {
 		return d.min;
 	}),
-	readingGroup = readingDimension.group().reduce(reduceAdd, reduceRemove, reduceInitial);
+	readingGroup = readingDimension.group().reduce(reduceAdd, reduceRemove, reduceInitial),
+	dayDimension = filter.dimension(function(d) {
+		return d.day;
+	}),
+	dayGroup = dayDimension.group().reduceSum(function(d) {
+		return 1;
+	}),
+	dragGroup = readingDimension.group().reduceSum(function(d) {
+		return 1;
+	});
 	function reduceAdd(p, v) {
 		++p.count;
 		p.temp += v.te;
 		p.light += v.li;
 		return p;
 	}
-
 	function reduceRemove(p, v) {
 		--p.count;
 		p.temp -= v.te;
 		p.light -= v.li;
 		return p;
 	}
-
 	function reduceInitial(p, v) {
 		return {count: 0, temp: 0, light: 0};
 	}
-	
-	roomChart
-	.dimension(roomDimension)
-	.group(roomGroup)
-	.colors(d3.scale.ordinal().range(color))
-	.title(function(d) {
-		return "Room: " + d.key + "\nReadings: " + d.value;
-	});
 
+	createGraph();
+}
+
+function createGraph() {
+	var numberFormat = d3.format('.2f');
 	readingChart
 	.renderArea(true)
-	.width(990)
+	.width($("#graph").width())
 	.height(200)
 	.transitionDuration(1000)
+	.rangeChart(dragChart)
 	.margins({top: 0, right: 50, bottom: 25, left: 40})
 	.dimension(readingDimension)
-	.x(d3.time.scale().domain(scale))
+	.x(d3.time.scale().domain(graphScale))
 	.round(d3.time.month.round)
 	.xUnits(d3.time.months)
 	.elasticY(true)
-	.yAxisLabel("KWh")
 	.brushOn(false)
 	.renderHorizontalGridLines(true)
 	.group(readingGroup)
 	.valueAccessor(function(p) {
 		if(sensor===0)
 			return p.value.count > 0 ? p.value.temp / p.value.count : 0; 
-		return p.value.count > 0 ? p.value.light / p.value.count : 0; 
+			return p.value.count > 0 ? p.value.light / p.value.count : 0; 
 	})
 	.title(function(p) {
-		var month = (p.key.getMonth())+1;
-		var year = (p.key.getYear())+1900;
-		var min = numberFormat(p.value.temp / p.value.count);
-		if(min==="NaN")
-			console.log("NAN")
-		var str = "Light level " + p.value.count > 0 ? p.value.light / p.value.count : 0 + " lux\nDate: ";
+		var formatTime = d3.time.format("%a %d %b - %H:%M");
 		if(sensor===0)
-			var str = "Temp level " + numberFormat(p.value.count > 0 ? p.value.temp / p.value.count : 0) + " \u00B0C\nDate: ";
-		return str + p.key.getDate() + "/" + month + "/" + year;
+			return "Temp level: " + numberFormat(p.value.count > 0 ? p.value.temp / p.value.count : 0) + " \u00B0C\nDate: " + formatTime(p.key);
+		return "Light level: " + (p.value.count > 0 ? p.value.light / p.value.count : 0) + " lux\nDate: " + formatTime(p.key);
 	});
 
-	dc.renderAll();
-	$("#graph").append("<br><hr><br>");
-}
+	roomChart
+	.dimension(roomDimension)
+	.group(roomGroup)
+	.colors(d3.scale.ordinal().range(color))
+	.title(function(d) {
+		return "Room: " + d.key + "\nReadings: " + d.value;
+	})
+	.valueAccessor(function(p) {
+		return 1;
+	});
 
-function getDate(d, day) {
-	var date = d.split("/");
-	date[0] = +date[0]+day
-	date[1] = date[1]-1;
-	if(date[1]===0)
-		date[1] = 12;
-	var s = new Date(date[2], date[1], date[0]);
-	return new Date(date[2], date[1], date[0]);
-}
-
-$("#change").onclick(function(d) {
+	dayChart
+	.dimension(dayDimension)
+	.group(dayGroup)
+	.elasticX(true)
+	.controlsUseVisibility(true)
+	.margins({top: 0, right: 5, bottom: -1, left: 0})
+	.valueAccessor(function(p) {
+		return 1;
+	})
+	.title(function(p) {
+		return "Readings: " + p.value;
+	})
+	.ordering(function(d) {
+		if(d.key === "Monday") return 0;
+		else if(d.key === "Tuesday") return 1;
+		else if(d.key === "Wednesday") return 2;
+		else if(d.key === "Thursday") return 3;
+		else if(d.key === "Friday") return 4;
+		else if(d.key === "Saturday") return 5;
+		else if(d.key === "Sunday") return 6;
+	});
 	
-	dc.filter();
+	dragChart
+	.height(60)
+    .width($("#graph").width())
+    .margins({top: 0, right: 50, bottom: 20, left: 50})
+    .dimension(readingDimension)
+    .group(dragGroup)
+    .valueAccessor(function(p) {
+		return 1;
+	})
+    .centerBar(true)
+    .gap(7)
+    .x(d3.time.scale().domain(graphScale))
+    .round(d3.time.minute.round)
+    .alwaysUseRounding(true)
+    .xUnits(d3.time.hours)
+    .xAxisLabel("Drag for Timeline")
+    .yAxis().ticks(0);
+    dragChart.xAxis().tickValues([]);
+
+	switchThermo.css({fill: "orange"});
+	switchBulb.css({fill: "none"});
+
+	dc.renderAll();
+}
+
+function save_first_order() {
+	var original_value = {}; // 1
+	return function(chart) {
+		chart.group().all().forEach(function(kv) { // 2
+			original_value[kv.key] = kv.value;
+		});
+		chart.ordering(function(kv) { // 3
+			return -original_value[kv.key];
+		});
+	};
+}
+
+$(document).ready(function() {
+	$(window).on("resize", function() {
+		readingChart
+		.width($("#graph").width())
+		.rescale();
+		dragChart
+		.width($("#graph").width())
+		.rescale();
+
+		dc.redrawAll();
+	});
+
+	switchThermo.on("click", function() {
+		if(sensor!=0) {
+			sensor = 0;
+			switchThermo.css({fill: "orange"});
+			switchBulb.css({fill: "none"});
+			dc.redrawAll();
+		}
+	});
+
+	switchBulb.on("click", function() {
+		if(sensor!=1) {
+			sensor = 1;
+			switchBulb.css({fill: "orange"});
+			switchThermo.css({fill: "none"});
+			dc.redrawAll();
+		}
+	});
 });
